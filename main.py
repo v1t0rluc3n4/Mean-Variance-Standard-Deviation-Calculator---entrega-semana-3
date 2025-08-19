dbd30c5daf68853b8a10c35712319bfab5ef57a5
Vitor Lucena <vitor.lucena@sou.inteli.edu.br>
2025-08-19 08:15:09 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,4 @@
+from mean_var_std import calculate
+
+if __name__ == "__main__":
+    print(calculate([0,1,2,3,4,5,6,7,8]))
---
+++ mean_var_std.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+def calculate(list_numbers):
+    if len(list_numbers) != 9:
+        raise ValueError("List must contain nine numbers.")
+    
+    matrix = np.array(list_numbers).reshape(3, 3)
+    
+    calculations = {
+        'mean': [matrix.mean(axis=0).tolist(), matrix.mean(axis=1).tolist(), matrix.mean().item()],
+        'variance': [matrix.var(axis=0).tolist(), matrix.var(axis=1).tolist(), matrix.var().item()],
+        'standard deviation': [matrix.std(axis=0).tolist(), matrix.std(axis=1).tolist(), matrix.std().item()],
+        'max': [matrix.max(axis=0).tolist(), matrix.max(axis=1).tolist(), matrix.max().item()],
+        'min': [matrix.min(axis=0).tolist(), matrix.min(axis=1).tolist(), matrix.min().item()],
+        'sum': [matrix.sum(axis=0).tolist(), matrix.sum(axis=1).tolist(), matrix.sum().item()]
+    }
+    
+    return calculations
